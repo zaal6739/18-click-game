43324ed7bad6afa01849de2d37cfbed6528fd520
Zack <zack1444@gmail.com>
2019-01-31 22:20:44 -0800

    Updates

---
+++ precache-manifest.9e3d936c1676cc3ac66812a3d218714c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "61dbe31c5f8e2fb06dd0",
+    "url": "/18-click-game/static/js/runtime~main.61dbe31c.js"
+  },
+  {
+    "revision": "61254b24158d1b557061",
+    "url": "/18-click-game/static/js/main.61254b24.chunk.js"
+  },
+  {
+    "revision": "8d0e6daf5a4074e9364a",
+    "url": "/18-click-game/static/js/1.8d0e6daf.chunk.js"
+  },
+  {
+    "revision": "710f0c4c9f5c744109b7e1b53e1838bf",
+    "url": "/18-click-game/index.html"
+  }
+];
